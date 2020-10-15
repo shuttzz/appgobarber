b8a4ea022ce1275d010f9bc612558dfb9518a3d5
José Assis de Menezes Neto <netomenezesucg@gmail.com>
2020-10-15 07:40:36 -0300

    Finalizada aula 1 do módulo 3 do nível 5

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,9 +1,76 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
+} from './styles';
+import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+interface RouteParams {
+  providerId: string;
+}
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const CreateAppointment: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const { user } = useAuth();
+  const route = useRoute();
+  const { providerId } = route.params as RouteParams;
+  const [selectedProvider, setSelectedProvider] = useState(providerId);
+  const { goBack } = useNavigation();
+
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
   return (
-    <View />
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+
+      <ProvidersListContainer>
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item: provider }) => (
+            <ProviderContainer selected={}>
+              <ProviderAvatar source={{ uri: provider.avatar_url }} />
+              <ProviderName>{provider.name}</ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
+    </Container>
   );
 };
 
